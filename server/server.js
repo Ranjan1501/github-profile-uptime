33ef44ea1e4dc197e3bbdbeb5b7abb47ebc1513a
ranjan.thakur1 <ranjan.thakur@nielsen.com>
2026-01-14 01:02:31 +0530

    allow all origin

--- server/server.js
+++ server/server.js
@@ -9,9 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Configure CORS properly
 app.use(cors({
   origin: [
-    'https://github-profile-uptime0101.vercel.app',
-    'http://localhost:5173', 
-    'http://localhost:3000'
+    '*'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
